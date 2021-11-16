95445f242f14d87d4b6401b728aa60a7642ba02a
Pranjal N <pranjalnavale992@gmail.com>
2021-11-15 21:11:10 -0600

    Add project files.

---
+++ BaseChild/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseChild
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Area ar = new Area();
+            ar.calculatearea();
+            ar.calculateperimeter();
+            Circle cr = new Circle();
+            cr.calculatearea();
+            cr.calculateperimeter();
+            Area arcr = new Circle();
+            arcr.calculatearea();
+            arcr.calculateperimeter();
+            
+        }
+
+        public class Area
+        {
+
+            public  virtual void calculatearea()
+            {
+                Console.WriteLine("base class area");
+            }
+
+            public  void calculateperimeter()
+            {
+                Console.WriteLine("base class perimeter");
+            }
+        }
+
+        public class Circle : Area
+        {
+
+            public override void calculatearea()
+            {
+
+                Console.WriteLine("child class area");
+            }
+
+            public new void calculateperimeter()
+            {
+                Console.WriteLine("child class perimeter");
+            }
+        }
+    }
+}
---
+++ BaseInterfaceChildNormal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseInterfaceChildNormal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cr = new Circle();
+            cr.calculatearea();
+            cr.calculateperimeter();
+            
+        }
+
+        public interface IArea
+        {
+            void calculatearea();
+
+            void calculateperimeter();
+            
+        }
+
+        public interface ICircleArea
+        {
+            void calculatearea();          
+
+        }
+
+        public class Circle: IArea, ICircleArea
+        {
+            public void calculatearea()
+            {
+                Console.WriteLine("child class area");
+            }
+
+            public void calculateperimeter()
+            {
+                Console.WriteLine("child class perimeter");
+            }
+
+        } 
+
+
+    }
+}
